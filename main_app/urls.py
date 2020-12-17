0b6e7107240cbcea92490db806034e48a7134a78
Neroku Greatnier <ragnerikon@gmail.com>
2020-12-18 03:31:02 +0700

    tyr5

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,8 +4,8 @@ from main_app import views
 
 urlpatterns = [
     path('about', views.about, name='about'),
-    path("login/", views.login, name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('login/', views.login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('social-auth/', include('social_django.urls', namespace="social")),
     path('', views.index, name='home'),
 ]
